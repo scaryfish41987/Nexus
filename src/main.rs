96304f191359e96fa873bd91e8d5d81f2d0b3502
scaryfish <jaimemmendivil4@gmail.com>
2026-04-17 22:34:27 -0700

    RustRover

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,3 @@
-mod algorithms;
-mod data;
-mod config;
-mod registry;
-mod metadata;
-mod app;
-mod render;
-
-use raylib::prelude::*;
-
 fn main() {
-    let (mut rl, thread) = raylib::init()
-        .size(640,500)
-        .title("Algoritmos y Estructuras de Datos")
-        .build();
-
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
-
-        d.clear_background(Color::WHITE);
-        d.draw_text("Hello World!",12,12,20, Color::BLACK);
-    }
+    println!("Hello, world!");
 }
-
